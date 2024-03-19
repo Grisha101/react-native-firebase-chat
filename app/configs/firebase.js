354f21579dfb3033e2f991d9c1519ab7be661a33
Luca <36565154+Grisha101@users.noreply.github.com>
2024-03-19 02:04:18 +0200

    some chngs

--- app/configs/firebase.js
+++ app/configs/firebase.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "<API_KEY>",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-  projectId: "<PROJECT_ID>",
-  storageBucket: "<BUCKET>.appspot.com",
-  messagingSenderId: "<SENDER_ID>",
+  apiKey: "AIzaSyC5w2Rb91S8FLyrvH0rOiJG-VMe0HSNbQM",
+  authDomain: "msg-01-105f8.firebaseapp.com",
+  projectId: "msg-01-105f8",
+  storageBucket: "msg-01-105f8.appspot.com",
+  messagingSenderId: "72423144855",
+  appId: "1:72423144855:web:c82a591e1eac1889bd18d2"
 }
+
